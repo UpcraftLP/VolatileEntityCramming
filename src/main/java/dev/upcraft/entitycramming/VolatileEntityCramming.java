5a91809282f2a14e772a2ac53c61edb42af9b1d5
UpcraftLP <j-schirra@gmx.de>
2020-11-26 13:57:22 +0100

    add logger

--- src/main/java/dev/upcraft/entitycramming/VolatileEntityCramming.java
+++ src/main/java/dev/upcraft/entitycramming/VolatileEntityCramming.java
@@ -2,9 +2,12 @@ package dev.upcraft.entitycramming;
 
 import io.github.glasspane.mesh.api.util.config.ConfigHandler;
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class VolatileEntityCramming implements ModInitializer {
 
+    public static final Logger LOGGER = LogManager.getLogger("VolatileEntityCramming");
     public static final String MODID = "vec";
 
     public static VecConfig getConfig() {
